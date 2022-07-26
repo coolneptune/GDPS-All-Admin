5008ae4f98701198e3248925f90baa28510b43b3
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-07-26 20:29:14 +0700

    Create addMod.php

--- tools/addMod.php
+++ tools/addMod.php
@@ -6,17 +6,10 @@ include "../incl/lib/connection.php";
 require "../incl/lib/generatePass.php";
 require_once "../incl/lib/exploitPatch.php";
 $ep = new exploitPatch();
-$userName = $ep->remove($_POST["userName"]);
 $accid = $ep->remove($_POST["accid"]);
 $role = $ep->remove($_POST["role"]);
-$password = $ep->remove($_POST["password"]);
 $TuserName = $ep->remove($_POST["TuserName"]);
-if(!empty($userName) AND !empty($password)){
-	$generatePass = new generatePass();
-	$pass = $generatePass->isValidUsrname($userName, $password);
-	if ($pass == 1) {
-
-	$query4 = $db->prepare("SELECT isAdmin FROM accounts WHERE userName=:userName");
+$query4 = $db->prepare("SELECT isAdmin FROM accounts WHERE userName=:userName");
         $query4->execute([':userName' => $userName]);
 		$query4 = $query4->fetchColumn();
 		if($query4 != 1) {
@@ -75,19 +68,7 @@ if(!empty($userName) AND !empty($password)){
 
 			echo "probably worked";
 
-	} 
-
-}else{
-		echo "Invalid password or nonexistant account. <a href='addMod.php'>Try again</a>";
 	}
-}else{
-	    echo "Admin Account";
-		echo '<form action="addMod.php" method="post">
-	Username: <input type="text" name="userName">
-	<br>
-	Password: <input type="password" name="password">
-	<br>';
-
 	echo "<br> Target Account <br> Use username <b>OR</b> Account ID, leave the other one empty <br>";
 
 	echo '<form action="addMod.php" method="post">
@@ -96,16 +77,13 @@ if(!empty($userName) AND !empty($password)){
 	Username: <input type"text" name="TuserName">
 	<br>';
 
-	echo "<br> Role <br>";
-	echo "0 = No Mod <br> 1 = Normal Mod <br> 2 = Elder Mod <br>";
+echo "<br> Role <br>";
+echo "0 = No Mod <br> 1 = Normal Mod <br> 2 = Elder Mod <br>";
 
-	echo '<form action="addMod.php" method="post">
+echo '<form action="addMod.php" method="post">
 	Role ID: <input type"number" name="role">
 	<br>
 	<br>
 	<input type="Submit" value="Submit">
 	</form>';
-
-
-}
 ?>
